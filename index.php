173a3132bb6bf63a00e07edc760e4e40d832150c
ashavargheeshini <90746717+ashavargheeshini@users.noreply.github.com>
2021-09-15 11:36:51 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,137 @@
+
+
+ <html>
+	<head>
+		<title>Card Class</title> 
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+		<link rel="stylesheet" href="bootstrap.min.css">
+	</head>
+	<body class="bg-info">
+		<!-- 	for large device -->
+		<div class="container d-md-none d-lg-block d-none">
+		
+			<div class="card-deck py-lg-5 py-md-5 my-lg-3">
+				<div class=" row  my-lg-5 py-lg-5">
+					<div class="col-lg-4 col-md-6 col-sm-12  mt-3"> 
+				
+						<div class="card ">
+							<img src="slider4.jpg" class=" img-fluid " alt="panda animal" >
+								<div class="card-body  mt-lg-5">
+									<h5 class="card-title text-center font-weight-bold">IMAGE 1</h5>
+									<p class="card-text ">A season is a division of the year based on changes in weather, ecology, and the number of daylight hours in a given region. On Earth, seasons are the result of Earth's orbit around the Sun and Earth's axial tilt relative to the ecliptic.</p>
+								</div>
+						</div> 
+					</div>
+					<div class="col-lg-4 col-md-6 col-sm-12 mt-3"> 
+						<div class="card ">
+							<img src="slider3.jpg" class=" img-fluid  " alt="dog animal">
+								<div class="card-body  mt-lg-5">
+									<h5 class="card-title text-center font-weight-bold">IMAGE 2</h5>
+									<p class="card-text">A season is a division of the year based on changes in weather, ecology, and the number of daylight hours in a given region. On Earth, seasons are the result of Earth's orbit around the Sun and Earth's axial tilt relative to the ecliptic.</p>
+								</div>
+						</div>
+					</div> 
+					<div class="col-lg-4 col-md-6  col-sm-12 mt-3"> 
+						<div class="card ">
+							<img src="slider6.jpg" class=" img-fluid" alt="dog animal">
+								<div class="card-body  mt-lg-5">
+									<h5 class="card-title text-center font-weight-bold">IMAGE 3</h5>
+									<p class="card-text">A season is a division of the year based on changes in weather, ecology, and the number of daylight hours in a given region. On Earth, seasons are the result of Earth's orbit around the Sun and Earth's axial tilt relative to the ecliptic.</p>
+								</div>
+						</div>
+					</div> 
+				</div>
+			</div>
+		</div>
+		
+		<!-- for medium device -->
+		<div class="container d-none d-md-block d-lg-none">
+			<div class="card-deck pt-md-3">
+				<div class="card ">
+					<div class="row text-center ">
+						<div class="col-md-6 py-3">
+							<img src="slider4.jpg" class="image-fluid card-img card-img-top  pl-3" alt="...">
+						</div>
+						<div class="col-md-6">
+							<div class="card-body ">
+								<h5 class="card-title text-center font-weight-bold">IMAGE 1</h5>
+								<p class="card-text overflow-auto"> A season is a division of the year based on changes in weather, ecology, and the number of daylight hours in a given region. On Earth, seasons are the result of Earth's orbit around the Sun and Earth's axial tilt relative to the ecliptic.</p>
+							</div>
+						</div>
+					</div>
+				</div>
+				</div>
+			
+				<div class="card-deck pt-md-3">
+				<div class="card ">
+					<div class="row text-center ">
+						<div class="col-md-6 py-3">
+							<img src="slider3.jpg" class="image-fluid card-img card-img-top  pl-3" alt="...">
+						</div>
+						<div class="col-md-6">
+							<div class="card-body ">
+								<h5 class="card-title text-center font-weight-bold">IMAGE 2</h5>
+								<p class="card-text overflow-auto"> A season is a division of the year based on changes in weather, ecology, and the number of daylight hours in a given region. On Earth, seasons are the result of Earth's orbit around the Sun and Earth's axial tilt relative to the ecliptic.</p>
+							</div>
+						</div>
+					</div>
+				</div>
+				</div>
+				<div class="card-deck pt-md-3">
+				<div class="card ">
+					<div class="row text-center ">
+						<div class="col-md-6 py-3">
+							<img src="slider6.jpg" class="image-fluid card-img card-img-top  pl-3" alt="...">
+						</div>
+						<div class="col-md-6">
+							<div class="card-body ">
+								<h5 class="card-title text-center font-weight-bold">IMAGE 3</h5>
+								<p class="card-text overflow-auto"> A season is a division of the year based on changes in weather, ecology, and the number of daylight hours in a given region. On Earth, seasons are the result of Earth's orbit around the Sun and Earth's axial tilt relative to the ecliptic. </p>
+							</div>
+						</div>
+					</div>
+				</div>
+				</div>
+				</div>
+
+		<!-- 		for small device -->
+		 <div class="container d-lg-none d-md-none">
+		
+			<div class="card-deck pt-3">
+				<div class=" row  ">
+					<div class="col-sm-12 order-2"> 
+				
+						<div class="card ">
+							<img src="slider4.jpg" class=" img-fluid " alt="panda animal" >
+								<div class="card-body ">
+									<h5 class="card-title text-center font-weight-bold">IMAGE 1</h5>
+									<p class="card-text ">A season is a division of the year based on changes in weather, ecology, and the number of daylight hours in a given region. On Earth, seasons are the result of Earth's orbit around the Sun and Earth's axial tilt relative to the ecliptic.</p>
+								</div>
+						</div> 
+					</div>
+					<div class="col-sm-12 order-3 "> 
+						<div class="card ">
+							<img src="slider3.jpg" class=" img-fluid  " alt="dog animal">
+								<div class="card-body  ">
+									<h5 class="card-title text-center font-weight-bold">IMAGE 2</h5>
+									<p class="card-text">A season is a division of the year based on changes in weather, ecology, and the number of daylight hours in a given region. On Earth, seasons are the result of Earth's orbit around the Sun and Earth's axial tilt relative to the ecliptic.</p>
+								</div>
+						</div>
+					</div> 
+					<div class="col-sm-12 order-1"> 
+						<div class="card ">
+							<img src="slider6.jpg" class=" img-fluid" alt="dog animal">
+								<div class="card-body  ">
+									<h5 class="card-title text-center font-weight-bold">IMAGE 3</h5>
+									<p class="card-text">A season is a division of the year based on changes in weather, ecology, and the number of daylight hours in a given region. On Earth, seasons are the result of Earth's orbit around the Sun and Earth's axial tilt relative to the ecliptic.</p>
+								</div>
+						</div>
+					</div> 
+				</div>
+			</div>
+		</div>
+	</body>
+</html>
+		
+	
+ 
